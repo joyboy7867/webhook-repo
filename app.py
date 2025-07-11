6b958f7422536bacfc09d808efc09fca764d952d
Shaikh Amaan <113681911+joyboy7867@users.noreply.github.com>
2025-07-11 19:53:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -78,4 +78,5 @@ def get_events():
     return jsonify(events)
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 locally
+    app.run(host="0.0.0.0", port=port)
